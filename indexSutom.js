649df249851b594b8f0bd419e51ff8e80f9ea08c
D0sal <oliveirada@eisti.eu>
2022-10-02 13:37:41 +0200

    tried install jsonwebtoken

--- indexSutom.js
+++ indexSutom.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const session = require('express-session')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 
 const app = express()
