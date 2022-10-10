06e09469f72b7e1e2b4592be1ba5c8c8f53ca545
D0sal <oliveirada@eisti.eu>
2022-10-10 20:49:47 +0200

    new Score microservice needs data to show..

---
+++ scoreAPI.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const port = 5001
+
+// make req.body from POST usefull
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serving public score file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/score.html')
+})
+
+// send user's data
+app.get('/scores', (req,res)=> {
+    /*
+    HERE FIND THE CURRENT'S USER DATAS
+    */
+
+    // simple solution for test
+    scores = [{mot:'oui',nbEssais:5},{mot:'non',nbEssais:9}]
+    
+    res.json(scores)
+})
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`)
+})
